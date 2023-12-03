427a6d2cee0bb23eeec1d4c69508c9d5a1e913a9
Gamedemons <72032280+Gamedemons@users.noreply.github.com>
2023-12-03 21:42:31 +0530

    added license and location prompt

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const chalk = require("chalk");
 const gradient = require("gradient-string");
 const figlet = require("figlet");
 var ProgressBar = require("progress");
-const readline = require("readline");
-
 const fs = require("fs");
 const axios = require("axios");
 const JSSoup = require("jssoup").default;
@@ -14,15 +12,15 @@ const htmlEntities = require("html-entities");
 
 // Variables
 const INFO_URL = "https://slider.kz/vk_auth.php?q=";
-const DOWNLOAD_URL = "https://slider.kz/download/";
+// const DOWNLOAD_URL = "https://slider.kz/download/";
 let index = -1;
 let songsList = [];
 let total = 0;
 let notFound = [];
 
 // Playlist URL
-let url = "https://m.resso.com/Zs8RftAta/";
-// let url = "https://www.resso.com/playlist/Favorite-songs-6950567530529855490";
+let url = "";
+let save_location = ""
 
 // CR - Connection Refused
 const connection_refused = chalk.hex("F18501");
@@ -33,123 +31,38 @@ const unescaped_char = chalk.hex("F18501");
 
 function header() {
   console.clear();
-  figlet(`Redify`, (err, data) => {
+  figlet(`Ressdify`, (err, data) => {
     console.log(gradient.pastel.multiline(data) + "\n");
     console.log(
       gradient.pastel.multiline(" A Resso Downloader - by Gamedemons") + "\n"
     );
   });
 }
+async function askUrl() {
+  const answers = await inquirer.prompt({
+    name: "url",
+    type: "input",
+    message: "Enter the resso playlist url : ",
+    default() {
+      return "https://m.resso.com/Zs8RftAta/";
+    },
+  });
 
-const download = async (song, url) => {
-  try {
-    let numb = index + 1;
-    console.log(`\n(${numb}/${total}) Starting download: ${song}`);
-    const { data, headers } = await axios({
-      method: "GET",
-      url: url,
-      responseType: "stream",
-    });
+  url = answers.url;
+}
+async function askSaveLocation() {
+  const answers = await inquirer.prompt({
+    name: "location",
+    type: "input",
+    message: "Enter the download location : ",
+    default() {
+      return "";
+    },
+  });
 
-    // Progress bar...
-    const totalLength = headers["content-length"];
-    const progressBar = new ProgressBar(
-      "-> downloading [:bar] :percent :etas",
-      {
-        width: 40,
-        complete: "=",
-        incomplete: " ",
-        renderThrottle: 1,
-        total: parseInt(totalLength),
-      }
-    );
+  save_location = answers.location;
+}
 
-    data.on("data", (chunk) => progressBar.tick(chunk.length));
-    data.on("end", () => {
-      startDownloading(); //for next song!
-    });
-
-    // Saving File
-    data.pipe(fs.createWriteStream(`${__dirname}/songs/${song}.mp3`));
-  } catch {
-    console.log(connection_refused("\nError - Connection refused"));
-    startDownloading();
-  }
-};
-
-const getURL = async (song, singer) => {
-  try {
-    let query = (song + "%20" + singer).replace(/\s/g, "%20");
-    const { data } = await axios.get(INFO_URL + query);
-
-    // when no result then [{}] is returned so length is always 1, when 1 result then [{id:"",etc:""}]
-    if (!data["audios"][""][0].id) {
-      // No result
-      console.log(not_found("\nError - Song not found : " + song));
-      notFound.push(song + " - " + singer);
-      startDownloading();
-      return;
-    }
-
-    //avoid remix,revisited,mix
-    let i = 0;
-    let track = data["audios"][""][i];
-    let totalTracks = data["audios"][""].length;
-    while (
-      i < totalTracks &&
-      /remix|revisited|reverb|mix/i.test(track.tit_art)
-    ) {
-      i += 1;
-      track = data["audios"][""][i];
-    }
-    //if reach the end then select the first song
-    if (!track) {
-      track = data["audios"][""][0];
-    }
-
-    if (fs.existsSync(__dirname + "/songs/" + track.tit_art + ".mp3")) {
-      let numb = index + 1;
-      console.log(
-        "\n(" + numb + "/" + total + ") - Song already present : " + song
-      );
-      startDownloading();
-      return;
-    }
-
-    let link = track.url;
-    link = encodeURI(link); // Replace unescaped characters
-
-    let songName = track.tit_art;
-    songName.replace(/\?|<|>|\*|\"|:|\||\/|\\/g, ""); // Removing special characters
-    download(songName, link);
-  } catch {
-    console.log(unescaped_char("\nError - Unescaped character : " + song));
-    startDownloading();
-  }
-};
-
-const startDownloading = () => {
-  try {
-    index += 1;
-    if (index === songsList.length) {
-      console.log(chalk.green("\n\nAll Songs Downloaded\n"));
-      console.log(chalk.yellow("Songs that are not found :"));
-      let i = 1;
-      for (let song of notFound) {
-        console.log(`${i} - ${song}`);
-        i += 1;
-      }
-      if (i === 1) console.log("None!");
-      return;
-    }
-
-    let song = songsList[index].name;
-    let singer = songsList[index].singer;
-    getURL(song, singer);
-  } catch {
-    console.log(chalk.red("Directory Error"));
-  }
-};
 
 async function getPlaylist() {
   try {
@@ -190,31 +103,121 @@ async function getPlaylist() {
   }
 }
 
-async function askName() {
-  const answers = await inquirer.prompt({
-    name: "url",
-    type: "input",
-    message: "Enter the resso playlist url : ",
-    default() {
-      return "";
-    },
-  });
+const startDownloading = () => {
+  try {
+    index += 1;
+    if (index === songsList.length) {
+      console.log(chalk.green("\n\nAll Songs Downloaded\n"));
+      console.log(chalk.yellow("Songs that are not found :"));
+      let i = 1;
+      for (let song of notFound) {
+        console.log(`${i} - ${song}`);
+        i += 1;
+      }
+      if (i === 1) console.log("None!");
+      return;
+    }
 
-  url = answers.url;
-}
+    let song = songsList[index].name;
+    let singer = songsList[index].singer;
+    getURL(song, singer);
+  } catch {
+    console.log(chalk.red("Directory Error"));
+  }
+};
+
+const getURL = async (song, singer) => {
+  let number = index + 1;
+  try {
+    let query = (song + "%20" + singer).replace(/\s/g, "%20");
+    const { data } = await axios.get(INFO_URL + query);
+
+    // when no result then [{}] is returned so length is always 1, when 1 result then [{id:"",etc:""}]
+    if (!data["audios"][""][0].id) {
+      // No result
+      console.log(not_found(`\n(${number}/${total}) Error - Song not found : ` + song));
+      notFound.push(song + " - " + singer);
+      startDownloading();
+      return;
+    }
+
+    //avoid remix,revisited,mix
+    let i = 0;
+    let track = data["audios"][""][i];
+    let totalTracks = data["audios"][""].length;
+    while (
+      i < totalTracks &&
+      /remix|revisited|reverb|mix/i.test(track.tit_art)
+    ) {
+      i += 1;
+      track = data["audios"][""][i];
+    }
+    //if reach the end then select the first song
+    if (!track) {
+      track = data["audios"][""][0];
+    }
+
+    if (fs.existsSync(save_location + "/" + track.tit_art + ".mp3")) {
+      let number = index + 1;
+      console.log(`\n(${number}/${total}) Song already present : ` + song);
+      startDownloading();
+      return;
+    }
+
+    let link = track.url;
+    link = encodeURI(link); // Replace unescaped characters
+
+    let songName = track.tit_art;
+    songName.replace(/\?|<|>|\*|\"|:|\||\/|\\/g, ""); // Removing special characters
+    download(songName, link);
+  } catch {
+    console.log(unescaped_char(`\n(${number}/${total}) Error - Unescaped character : ` + song));
+    startDownloading();
+  }
+};
+
+const download = async (song, url) => {
+  let number = index + 1;
+  try {
+    console.log(`\n(${number}/${total}) Downloading: ${song}`);
+    const { data, headers } = await axios({
+      method: "GET",
+      url: url,
+      responseType: "stream",
+    });
+
+    // Progress bar...
+    const totalLength = headers["content-length"];
+    const progressBar = new ProgressBar(
+      " :bar :percent :etas",
+      {
+        width: 50,
+        complete: "■",
+        incomplete: " ",
+        renderThrottle: 1,
+        total: parseInt(totalLength),
+      }
+    );
+
+    data.on("data", (chunk) => progressBar.tick(chunk.length));
+    data.on("end", () => {
+      startDownloading(); //for next song!
+    });
+
+    // Saving File
+    data.pipe(fs.createWriteStream(`${save_location}\\${song}.mp3`));
+  } catch {
+    console.log(connection_refused(`(${number}/${total}) Error - Connection refused : ` + song));
+    startDownloading();
+  }
+};
 
 async function initialize() {
-  const logo_color = chalk.hex("FE0A6C");
-  console.log(chalk.greenBright.bold("Welcome to REDIFY !"))
+  console.clear();
+  console.log(chalk.greenBright.bold("Welcome to RESSDIFY !"))
 
-  await askName()
-  // const askUrl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-  // askUrl.question("Enter the resso playlist url : ", function (answer) {
-  //   url = answer;
-  // });
+  await askUrl();
+  await askSaveLocation();
 
   header();
 
